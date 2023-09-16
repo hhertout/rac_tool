c95cc1c3298a8713e68e09215d8be2ed42b12e90
hhertout <hugues.hertout@gmail.com>
2023-09-17 00:43:30 +0200

    Adding copy for yml on root dir

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,25 @@ use std::process;
 use initializer::Initializer;
 use logger::Logger;
 
+pub mod run;
+
 fn main() {
+    let filename = String::from("config.yml");
     let args: Vec<String> = env::args().collect();
     let main_arg = args[1].clone();
     mem::drop(args);
 
     match main_arg.as_str() {
         "init" => {
-            let filename = String::from("config.yml");
             let initializer = Initializer::new(filename.clone());
             match initializer.create_yml(None) {
                 Ok(_) => Logger::init_success(&filename, None),
                 Err(err) => Logger::init_failed(&err),
             };
+        }
+        "run" => {
+            let runner = run::Runner::new(filename.clone());
+            runner.run();
         }
         _ => {
             Logger::wrong_args();
--- tests/specs/read_yml.rs
+++ tests/specs/read_yml.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::{fs::read_dir, path::Path};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct YamlTest {
@@ -45,4 +46,37 @@ fn test_yml() {
 
     assert_eq!(split[0], "text.txt.example");
     assert_eq!(split[1], "text.txt");
+}
+
+#[test]
+fn test_dir() -> std::io::Result<()> {
+    let ignored_dir: Vec<&str> = vec![
+        "./target",
+        "./.git",
+        "./github",
+        "./node_modules",
+        "./vendor",
+        "./public",
+    ];
+    let path = Path::new(".");
+    if path.is_dir() {
+        for entry in read_dir(path)? {
+            match entry {
+                Ok(entry) => {
+                    if !ignored_dir.contains(&entry.path().as_os_str().to_str().unwrap()) {
+                        println!("{:?}", entry.path().file_name().unwrap());
+                    }
+                }
+                Err(err) => {
+                    println!("{}", err)
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn visit_dir_test() {
+    let path = Path::new("./tests");
 }
