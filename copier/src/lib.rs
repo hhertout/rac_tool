7376e955733100e1dc9a3344a6a1afb15d2f2da3
hhertout <hugues.hertout@gmail.com>
2023-09-16 19:20:54 +0200

    improve arch of crates

---
+++ copier/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod logger;
+
+pub struct Copier;
+
+impl Copier {
+    pub fn new() -> Copier {
+        Copier {}
+    }
+}
--- init/tests/init_test.rs
+++ init/tests/init_test.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
-use init::{Initializer, schema::Schema};
+use init::{schema::Schema, Initializer};
 
 #[test]
 pub fn create_yaml() {
     let filename = String::from("config_test.yml");
     let initializer = Initializer::new(filename.clone());
-    let result = initializer.create_yml(Some("./tests/specs/yaml/"));
+    let result = initializer.create_yml(Some("./tests/yaml/"));
     assert!(result.is_ok());
 
-    let path = "./tests/specs/yaml/".to_owned() + &filename;
+    let path = "./tests/yaml/".to_owned() + &filename;
     let content = fs::read_to_string(path);
     assert!(content.is_ok());
     let c = content.unwrap();
