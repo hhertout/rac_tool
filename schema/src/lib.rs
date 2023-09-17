e23320d1922a8003cc9cad93a8d5ef910ab17300
hhertout <hugues.hertout@gmail.com>
2023-09-17 11:22:59 +0200

    add ignored_dir and prevent failure cause of invalid configuration file

--- schema/src/lib.rs
+++ schema/src/lib.rs
@@ -3,14 +3,21 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Schema {
     pub on: String,
-    pub files: Vec<String>
+    pub files: Vec<String>,
+    pub ingored_dir: Option<Vec<String>>,
 }
 
 impl Schema {
     pub fn new() -> Schema {
         Schema {
             on: String::from("."),
-            files: vec![String::from("hello.txt.example:hello.txt")]
+            files: vec![String::from("hello.txt.example:hello.txt")],
+            ingored_dir: Some(vec![
+                String::from("/.git/"),
+                String::from("/node_modules/"),
+                String::from("/vendor/"),
+                String::from("/target/"),
+            ]),
         }
     }
 }
